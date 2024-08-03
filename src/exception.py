0184f21a49d8e02d34ba26c8bd3f08de07be1ee9
Deepaksingh <itsdeepaksingh00@gmail.com>
2024-08-03 10:49:14 +0530

    file updated

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,6 @@
 import sys
-import logging
+from src.logger import logging
+
 
 def error_message_detail(error, error_detail: sys):
     _, _, exc_tb = error_detail.exc_info()
@@ -12,16 +13,7 @@ def error_message_detail(error, error_detail: sys):
 class CustomException(Exception):
     def __init__(self, error_message, error_detail: sys):
         super().__init__(error_message)
-        self.error_message = error_message_detail(error_message, error_detail=error_detail)
+        self.error_message = error_message_detail(error_message, error_detail)
 
     def __str__(self):
         return self.error_message
-
-if __name__ == "__main__":
-    logging.basicConfig(level=logging.ERROR)  # Set logging level to ERROR
-
-    try:
-        a = 1 / 0
-    except Exception as e:
-        logging.error("An error occurred")
-        raise CustomException(e, sys)
